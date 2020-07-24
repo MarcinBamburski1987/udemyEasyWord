87630b45604d5cc8278d9bb6410f29609d2dce87
MarcinBamburski1987 <marcin2511@op.pl>
2020-07-24 18:39:55 +0200

    fetchWord method implementhed in question.component.ts and ivnoke in ngOnInit. This component has word field

--- src/app/components/question/question.component.ts
+++ src/app/components/question/question.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { WordsService } from '../../services/words.service';
+import { WordType } from '../../data/models';
 
 @Component({
   selector: 'app-question',
@@ -7,9 +9,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class QuestionComponent implements OnInit {
 
-  constructor() { }
+  word: WordType = null;
+
+  constructor(private wordsService: WordsService) { }
 
   ngOnInit(): void {
+    this.fetchWord();
+  }
+  private fetchWord(): void{
+    this.word = this.wordsService.getWords().shift();
   }
 
 }
